ad3ab3380efdd54e80c55fa5c9cd06482315cc16
Arthur Diniz <arthurvdinizs@gmail.com>
2018-05-02 17:10:37 -0300

    para daniel pegar

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>ola</v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
---
+++ src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="content">
+    <Header/>
+  </main>
+</template>
+
+<script>
+import Header from '@/components/shared/Header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
