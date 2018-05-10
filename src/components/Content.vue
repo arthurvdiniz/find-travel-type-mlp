726db7916e2d92de26cd4feb1689f806ddaf3b84
Arthur Diniz <arthurvdinizs@gmail.com>
2018-05-10 17:38:11 -0300

    interface quase finalizada

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -5,8 +5,9 @@
       <p class="text">teste</p>
     </div>
     <div class="content">
-      <form-caracteristicas @clickedBtn="startNN($event)" v-if="true"/>
-      <rocket v-if="false"/>
+      <form-caracteristicas @clickedBtn="startNN($event)" v-if="!isLoading && !ended"/>
+      <rocket v-if="isLoading"/>
+      <resultado :result="result" :ended="ended" @goBack="ended = false; result = []" v-if="ended"/>
     </div>
   </div>
 </template>
@@ -14,23 +15,40 @@
 <script>
 import FormCaracteristicas from '@/components/FormCaracteristicas'
 import Rocket from '@/components/Rocket'
+import Resultado from '@/components/Resultado'
 import NeuralNetwork from '@/js/neuralNetwork'
 
 const N_INPUTS = 8
 const N_OUTPUT = 5
 const N_NEURONIOS = 10
-const LEARNING_RATE = 0.1
+const LEARNING_RATE = 0.5
 
 export default {
   components: {
     FormCaracteristicas,
-    Rocket
+    Rocket,
+    Resultado
+  },
+  data () {
+    return {
+      isLoading: false,
+      ended: false,
+      result: []
+    }
   },
   methods: {
     startNN (inputs) {
+      console.log(inputs)
+      this.ended = false
+      this.isLoading = true
       let nn = new NeuralNetwork(N_INPUTS, N_NEURONIOS, N_OUTPUT, LEARNING_RATE)
+      this.result = nn.feedForward(inputs)
+      console.log(this.result)
       console.log(nn)
-      console.log(nn.feedForward(inputs))
+      setTimeout(() => {
+        this.isLoading = false
+        this.ended = true
+      }, 5000)
     }
   }
 }
---
+++ src/components/Resultado.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="wrapper-result">
+    <h1 class="type-travel">{{travel.type}}</h1>
+    <h3 class="description-travel">{{travel.description}}</h3>
+    <p class="output">{{result}}</p>
+    <v-btn fab dark color="black" class="btn-restart" @click="back()">
+      <v-icon>mdi-refresh</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['result', 'ended'],
+  data () {
+    return {
+      travel: {
+        type: '',
+        description: ''
+      }
+    }
+  },
+  mounted () {
+    let highest = this.result[0]
+    let position = 0
+    this.result.forEach((el, index) => {
+      if (el > highest) {
+        highest = el
+        position = index
+      }
+    })
+    this.travel = this.typesTravel[position]
+  },
+  methods: {
+    back () {
+      this.$emit('goBack')
+    }
+  },
+  computed: {
+    typesTravel () {
+      return [
+        {
+          type: 'Colonização',
+          description: 'Reune as principais características citadas'
+        },
+        {
+          type: 'Expedição',
+          description: 'Descoberta de outros tipos de seres vivos'
+        },
+        {
+          type: 'Exploração',
+          description: 'Resultados inconclusivos'
+        },
+        {
+          type: 'Mineração',
+          description: 'Obtenção de metais preciosos'
+        },
+        {
+          type: 'Ciência',
+          description: 'Descoberta de novos materiais'
+        },
+      ]
+    }
+  }
+}
+</script>
