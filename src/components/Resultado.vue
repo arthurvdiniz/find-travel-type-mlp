aaeed82de539c5fd5514908c7b3554d343328327
Arthur Diniz <arthurvdinizs@gmail.com>
2018-05-10 18:15:39 -0300

    cores nos icones do resultado

--- src/components/Resultado.vue
+++ src/components/Resultado.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper-result">
-    <v-icon class="icon-travel" size="75" color="black">{{travel.icon}}</v-icon>
+    <v-icon class="icon-travel" size="75" :color="travel.color">{{travel.icon}}</v-icon>
     <h1 class="type-travel">{{travel.type}}</h1>
     <h3 class="description-travel">{{travel.description}}</h3>
     <p class="output">{{result}}</p>
@@ -43,27 +43,32 @@ export default {
         {
           type: 'Colonização',
           description: 'Reune as principais características citadas',
-          icon: 'mdi-home-variant'
+          icon: 'mdi-home-variant',
+          color: 'light-blue darken-3'
         },
         {
           type: 'Expedição',
           description: 'Descoberta de outros tipos de seres vivos',
-          icon: 'mdi-human-greeting'
+          icon: 'mdi-human-greeting',
+          color: 'green darken-3'
         },
         {
           type: 'Exploração',
           description: 'Resultados inconclusivos',
-          icon: 'mdi-magnify'
+          icon: 'mdi-magnify',
+          color: 'red darken-3'
         },
         {
           type: 'Mineração',
           description: 'Obtenção de metais preciosos',
-          icon: 'mdi-barrel'
+          icon: 'mdi-barrel',
+          color: 'amber darken-3'
         },
         {
           type: 'Ciência',
           description: 'Descoberta de novos materiais',
-          icon: 'mdi-flask'
+          icon: 'mdi-flask',
+          color: 'purple darken-3'
         },
       ]
     }
