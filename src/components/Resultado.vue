0d0dd782dabffd1156f1000710f15610d03e1533
Arthur Diniz <arthurvdinizs@gmail.com>
2018-05-14 09:15:21 -0300

    ajustado style

--- src/components/Resultado.vue
+++ src/components/Resultado.vue
@@ -44,7 +44,7 @@ export default {
       return [
         {
           type: 'Colonização',
-          description: 'Reune as principais características citadas',
+          description: 'Reúne as principais características citadas',
           icon: 'mdi-home-variant',
           color: 'light-blue darken-3'
         },
